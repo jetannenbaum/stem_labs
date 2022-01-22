ef3c023271fdeb05741f83c95488516c92dcbe7c
Jim Tannenbaum <jetannenbaum@gmail.com>
2022-01-22 13:40:11 -0500

    Add files via upload

--- lab6.py
+++ lab6.py
@@ -1,37 +1,19 @@
 # Author: J.E. Tannenbaum
-# Initial Release: 01/17/2022
-# Playing with a neopixel bar
+# Initial Release: 01/22/2022
+# Night Light LED
 
-import time
-# We are using https://github.com/blaz-r/pi_pico_neopixel
-from neopixel import Neopixel
+from machine import Pin, ADC
+from time import sleep
 
-NUMBER_PIXELS = 8
-STATE_MACHINE = 0
-LED_PIN = 0
+# Define the pin for the A/D Converter
+photo = machine.ADC(26)
 
-# We are using the GRB variety, not RGB
-strip = Neopixel(NUMBER_PIXELS, STATE_MACHINE, LED_PIN, "GRB")
-# set 100% brightness
-strip.brightness(100)
-delay = .1
-
-# Colors from https://www.rapidtables.com/web/color/RGB_Color.html
-white = (255, 255, 255)
-red = (255, 0, 0)
-orange = (255, 165, 0)
-yellow = (255, 255, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
-indigo = (75, 0, 130)
-violet = (148, 0,211)
-black = (0, 0, 0)
-colors = (black, white, red, orange, yellow, green, blue, indigo, violet, black)
+# Define the pin for the led
+led = Pin(16, Pin.OUT)
 
 while True:
-    for color in colors:
-        for i in range(NUMBER_PIXELS):
-            strip.set_pixel(i, color)
-            strip.show()
-            time.sleep(delay)
-        
+    if photo.read_u16() > 30000:
+        led.on()
+    else:
+        led.off()
+    sleep(.5)
